0437152fe7a59aefcb0a9467126a81f2f16b67b1
David Wang <dwang4211@gmail.com>
2020-06-21 19:56:22 -0400

    Added confirm page, small validation features

--- app/athlete-form/athlete-form.module.js
+++ app/athlete-form/athlete-form.module.js
@@ -12,7 +12,35 @@ angular.module('athleteProfileApp.athleteForm', ['ngRoute'])
 .controller('athleteFormController', ['$http', function($http) {
   let self = this
   self.pendingForm = {gender: 'Male'}
+  self.confirm = false
+  self.error = false
+
   $http.get('http://localhost:3000/api/sports').then(function(response) {
     self.SPORTS = response.data
   })
+
+  self.validate = function() {
+    if (!(self.pendingForm.name && self.pendingForm.gender && self.pendingForm.dob)) {
+      return false
+    }
+
+    //TODO: NEED TO VALIDATE DATE
+    if (self.pendingForm.dob) {}
+    return true
+  }
+  self.goConfirm = function() {
+    self.error = false
+
+
+    if (self.validate()) {
+      self.confirm = true
+    }
+    else {
+      self.error = true
+    }
+  }
+  self.goBack = function() {
+    self.confirm = false
+  }
+
 }]);
